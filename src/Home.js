0d09f784ea5f0b50394248119c96b9522d209771
Prathmesh Doddanawar <37949352+prathmesh3235@users.noreply.github.com>
2023-05-29 23:57:52 +0200

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,30 @@
+import React,{useRef} from 'react';
+import styled from 'styled-components';
+import HeroSection from './Components/HeroSection';
+import ProductDisplay from './Components/ProductDisplay';
+import Virtualtryon from './Components/Virtualtryon';
+import ProductList from './Components/ProductList';
+import App from './App';
+import Header from './Components/Header';
+
+const Home=({ref})=>{
+
+    
+    const data = 
+            {
+        name: "Sunny",
+        description: "This is a description for the home page",
+            };
+    return <>
+    <HeroSection myData = {data} />
+    <div className="pcontainer">
+    <ProductList ref={ref} />
+    </div>
+    
+    
+    </>;
+};
+
+
+
+export default Home;
