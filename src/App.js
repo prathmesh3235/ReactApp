ad6eafe11dbae2aaf841fbf01da9a719c2db9415
Prathmesh Doddanawar <37949352+prathmesh3235@users.noreply.github.com>
2023-06-10 16:17:15 +0200

    Tracking ID changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Moreinfo from "./moreinfo";
 import Login from "./login";
 import ReactGA from "react-ga4";
 
-const TRACKING_ID = "G-D372R0870T";  
+const TRACKING_ID = "UA-270359891-2";  
 ReactGA.initialize(TRACKING_ID);
 
 
