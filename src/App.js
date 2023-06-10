dc8b6abee43b03bf77c81a92214d0892b70e14df
Prathmesh Doddanawar <37949352+prathmesh3235@users.noreply.github.com>
2023-06-10 16:37:07 +0200

    Users 4

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Moreinfo from "./moreinfo";
 import Login from "./login";
 import ReactGA from "react-ga4";
 
-const TRACKING_ID = "G-G7L7Q28LTL";  
+const TRACKING_ID = "G-J5ZPGPNX5E";  
 ReactGA.initialize(TRACKING_ID);
 
 
