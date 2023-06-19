1e9144dba06c8670f2c29b11f8f2e8d6007bd4ca
Prathmesh Doddanawar <37949352+prathmesh3235@users.noreply.github.com>
2023-06-19 16:38:05 +0200

    virtual try on edit

--- src/Components/Virtualtryon.js
+++ src/Components/Virtualtryon.js
@@ -24,9 +24,9 @@ const Virtualtryon = ({userId,product}) => {
     <div className="virtual-container">
         {/* <h2 className="virtual-text"> <BsEmojiSunglasses /> Probiere {product.product_name} virtuell an</h2> */}
         <div className="vertical-center">
-        <a onClick={handleClick} href={'https://virtual-tryon-five.vercel.app/?sku=' + product.sku} target='blank'>
+        <a onClick={handleClick} href={'https://virtual-tryon-five.vercel.app/?sku=' +product.sku} target='blank'>
           <Button> <BsCameraFill /> Try On Virtually</Button></a>
-          </div>
+          </div> 
     
     {/* <div className="Iframe">
       <h2>Iframe</h2>
